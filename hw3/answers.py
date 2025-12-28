eb8e4b291ef94e8eae9a3563f3ec9fb0d28f54eb
amitarad36 <amitarad36@gmail.com>
2025-12-28 08:52:13 +0200

    done with rnn - need to answer questions

--- hw3/answers.py
+++ hw3/answers.py
@@ -39,7 +39,8 @@ def part1_generation_params():
     temperature = 0.0001
     # TODO: Tweak the parameters to generate a literary masterpiece.
     # ====== YOUR CODE: ======
-    pass
+    start_seq = "For i am just:\n"
+    temperature = 0.8
     # ========================
     return start_seq, temperature
 
